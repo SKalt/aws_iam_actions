38d2ee9e524f912653044c7b44324c96b05dc44e
Steven Kalt <kalt.steven@gmail.com>
2024-01-30 09:57:02 -0500

    feat(website): monospace font for prefixes, actions

--- website/src/components/SearchResults.tsx
+++ website/src/components/SearchResults.tsx
@@ -37,9 +37,10 @@ function SearchResult({ result, q }: { result: Result; q: string }) {
   const link =
     result.link || `/${result.kind}/${encodeURIComponent(result.name)}`;
   const target = result.kind === "action" ? "_blank" : "_self";
+  const font = result.kind === "service" ? "" : "font-mono";
   return (
     <li className="result container grid grid-cols-2">
-      <Link href={link} target={target} className="">
+      <Link href={link} target={target} className={font}>
         <HighlightedName name={result.name} q={q} />
       </Link>
       <span className="bg-info">{result.kind}</span>
